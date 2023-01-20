06e781a131abbbfb14c2a5431a83f1f6763d7793
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-01-20 10:30:06 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,20 +8,19 @@ startBtn.addEventListener('click', onStart);
 stopBtn.addEventListener('click', onStop);
 
 function onStart() {
-timerId = setInterval(() => {
-   body.style.backgroundColor = getRandomHexColor();
- }, 1000);
- startBtn.disabled = true;
- stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
 }
 
 function onStop() {
- clearInterval(timerId);
- startBtn.disabled = false;
- stopBtn.disabled = true;
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
 }
 
 function getRandomHexColor() {
- return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-console.log("Hello");
